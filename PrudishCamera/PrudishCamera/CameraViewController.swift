73a4090533e13a64284aadd6d4e3f27fa8abe774
ijinfeng <jinfeng@yunjigobal.com>
2021-08-04 23:11:13 +0800

    update

---
+++ PrudishCamera/PrudishCamera/CameraViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CameraViewController.swift
+//  PrudishCamera
+//
+//  Created by JinFeng on 2021/8/4.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        let device = AVCaptureDevice.default(for: .video)
+        
+    }
+    
+}
--- PrudishCamera/PrudishCamera/Setting/CameraSetting.swift
+++ PrudishCamera/PrudishCamera/Setting/CameraSetting.swift
@@ -20,6 +20,42 @@ class Setting: NSObject {
         super.init()
         decode(with: coder)
     }
+    
+    override init() {
+        super.init()
+    }
+    
+    func save() {
+        guard let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first else {
+            return
+        }
+        let savePath = URL(fileURLWithPath: documentPath).appendingPathComponent("prudish.camera.archive")
+        do {
+            let data = try NSKeyedArchiver.archivedData(withRootObject: self, requiringSecureCoding: true)
+            do {
+                try data.write(to: savePath)
+            } catch {
+                print("User setting save failed: \(error)")
+            }
+        } catch {
+            print("Archive data failed: \(error)")
+        }
+    }
+    
+    static func read() -> Setting {
+        guard let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first else {
+            return Setting()
+        }
+        let savePath = URL(fileURLWithPath: documentPath).appendingPathComponent("prudish.camera.archive")
+        do {
+            let data = try Data.init(contentsOf: savePath)
+            let model = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? Setting
+            return model ?? Setting()
+        } catch {
+            print("Read data failed: \(error)")
+        }
+        return Setting()
+    }
 }
 
 extension Setting: NSCoding, NSSecureCoding {
